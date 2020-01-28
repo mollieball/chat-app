15e2262cf030295546af43162a2dc54b88e73785
Mollie Ball <mollieball@squarepeg.ie>
2020-01-28 17:11:41 +0000

    create chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact
+        avatar="https://randomuser.me/api/portraits/women/94.jpg"
+        name="Samantha Cast"
+        online={true}
+      />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/women/45.jpg"
+        name="Jean Torres"
+        online={false}
+      />
+      <Contact
+        avatar="https://randomuser.me/api/portraits/men/39.jpg"
+        name="Jimmie Hansen"
+        online={false}
+      />
     </div>
   );
 }
