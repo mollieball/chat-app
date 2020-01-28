e1d7691a7939f13a7416634cea35274c28dc033a
Mollie Ball <mollieball@squarepeg.ie>
2020-01-28 16:03:32 +0000

    creat full chat app

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import './Contact.css';
 
-
 function Contact(props) {
     return (
-
-
-        <div className ="Contact">    
-    
-        <img src= {props.avatar} className= "avatar"/>
-    
-          
-    
-        <div>
-        <h4 class="name">{props.name}
-            </h4>
-        
-       <div className="status">
-           <div className= {props.online ? 'status-online' : 'status-offline'} />
-        <p className="status-text">
-        {props.online ? 'online' : 'offline'}</p>
-       </div>
+        <div className="Contact">
+            <img src={props.avatar} className="avatar" />
+            <div>
+                <h4 className="name">{props.name}
+                </h4>
+                <div className="status">
+                    <div className={props.online ? 'status-online' : 'status-offline'} />
+                    <span className="status-text">
+                        {props.online ? 'online' : 'offline'}</span>
+                </div>
+            </div>
         </div>
-    
-    
-    
-    </div>
-
-    )};
+    )
+};
 
 export default Contact;
