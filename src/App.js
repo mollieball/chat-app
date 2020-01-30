6da973e7ee32a1d10442be21365ba2a10e5b6443
Mollie Ball <mollieball@squarepeg.ie>
2020-01-30 09:12:20 +0000

    Add contactList to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
+      <ContactList/>
       <Contact
         avatar="https://randomuser.me/api/portraits/women/94.jpg"
         name="Samantha Cast"
